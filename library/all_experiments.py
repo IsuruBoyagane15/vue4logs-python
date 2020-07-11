f95bee958bd6da5a599ef415c41737468c9045ea
Isuru Boyagane <isuruboyagane15@gmail.com>
2020-07-11 18:36:15 +0530

    Update all_imports.py

---
+++ library/all_experiments.py
@@ -0,0 +1,42 @@
+ALL_EXPERIMENTS = {
+    17: "android",
+    18: "apache",
+    19: "bgl",
+    20: "hadoop",
+    21: "hdfs_1",
+    22: "healthapp",
+    23: "hpc",
+    24: "linux",
+    25: "mac",
+    26: "openstack",
+    27: "proxifier",
+    28: "spark",
+    29: "ssh",
+    30: "thunderbird",
+    31: "windows",
+    32: "zookeeper",
+}
+
+EXPERIMENT_LIB_MAPPING = {
+17: "android",
+    18: "apache",
+    19: "bgl",
+    20: "hadoop",
+    21: "hdfs_1",
+    22: "healthapp",
+    23: "hpc",
+    24: "linux",
+    25: "mac",
+    26: "openstack",
+    27: "proxifier",
+    28: "spark",
+    29: "ssh",
+    30: "thunderbird",
+    31: "windows",
+    32: "zookeeper",
+}
+
+SPLIT_TOKEN ={
+    "default":['.', '"' , "'" , ',' , '(', ')', '!', '?', ';', ':', "\\" , '/', '[',']',"=","-",'_' ],
+    10:['"'," "],
+}
